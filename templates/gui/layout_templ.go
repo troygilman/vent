b036bd015348d3a7d16ceac63efd72067495c86d
Troy Gilman <troygilman@gmail.com>
2025-11-21 08:42:56 -0500

    added dummy table

--- templates/gui/layout_templ.go
+++ templates/gui/layout_templ.go
@@ -97,7 +97,15 @@ func Layout(props LayoutProps) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "</ul></div></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "</ul></div><div class=\"flex grow min-w-0\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "</div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- templates/gui/schema_table_templ.go
+++ templates/gui/schema_table_templ.go
@@ -57,6 +57,10 @@ func SchemaTablePage(props SchemaTablePageProps) templ.Component {
 					}()
 				}
 				ctx = templ.InitializeContext(ctx)
+				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"w-full overflow-x-auto\"><table class=\"table\"><!-- head --><thead><tr><th></th><th>Name</th><th>Job</th><th>Favorite Color</th></tr></thead> <tbody><!-- row 1 --><tr><th>1</th><td>Cy Ganderton</td><td>Quality Control Specialist</td><td>Blue</td></tr><!-- row 2 --><tr><th>2</th><td>Hart Hagerty</td><td>Desktop Support Technician</td><td>Purple</td></tr><!-- row 3 --><tr><th>3</th><td>Brice Swyre</td><td>Tax Accountant</td><td>Red</td></tr></tbody></table></div>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
 				return nil
 			})
 			templ_7745c5c3_Err = Layout(props.LayoutProps).Render(templ.WithChildren(ctx, templ_7745c5c3_Var3), templ_7745c5c3_Buffer)
