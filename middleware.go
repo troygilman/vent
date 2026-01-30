7880c48ed514674e824d236f436bce9939fdd8cf
Troy Gilman <troygilman@gmail.com>
2026-01-30 18:37:00 -0500

    middleware

--- middleware.go
+++ middleware.go
@@ -3,8 +3,12 @@ package vent
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"vent/auth"
 )
+
+type claimsContextKey struct{}
+type userContextKey struct{}
 
 type Middleware = func(http.Handler) http.Handler
 
@@ -24,8 +28,47 @@ func NewAuthMiddleware(secretProvider auth.SecretProvider) Middleware {
 				return
 			}
 
-			ctx := r.Context()
-			ctx = context.WithValue(ctx, "claims", claims)
+			ctx := context.WithValue(r.Context(), claimsContextKey{}, claims)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func AuthorizationMiddleware(permissions []string) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user, ok := r.Context().Value(userContextKey{}).(EntityData)
+			if !ok {
+				http.Error(w, "user not found in context", http.StatusInternalServerError)
+			}
+			_ = user
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func UserMiddleware(schema SchemaConfig) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			claims, ok := r.Context().Value(claimsContextKey{}).(auth.VentClaims)
+			if !ok {
+				http.Error(w, "claims not present", http.StatusForbidden)
+				return
+			}
+
+			userID, err := strconv.Atoi(claims.ID)
+			if err != nil {
+				http.Error(w, "claims ID is not an int", http.StatusInternalServerError)
+			}
+
+			entity, err := schema.Client.Get(r.Context(), userID)
+			if err != nil {
+				http.Error(w, "could not find user", http.StatusInternalServerError)
+			}
+
+			ctx := context.WithValue(r.Context(), userContextKey{}, entity)
 			r = r.WithContext(ctx)
 			next.ServeHTTP(w, r)
 		})
