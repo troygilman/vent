b5324c3346fde48244eae8b54f6db383944d575a
Troy Gilman <troygilman@gmail.com>
2026-05-13 10:58:15 -0400

    Add Cache-Control header to static file handler

--- handlers.go
+++ handlers.go
@@ -10,5 +10,9 @@ var static embed.FS
 
 // StaticDirHandler returns an HTTP handler that serves static files embedded in the vent package.
 func StaticDirHandler() http.Handler {
-	return http.FileServerFS(static)
+	fileServer := http.FileServerFS(static)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=3600")
+		fileServer.ServeHTTP(w, r)
+	})
 }
